d0245825c5bf92a7a86211de91a6bcb4813d9ee0
azinuddinazlani <azinuddinazlani@gmail.com>
2025-02-18 14:53:48 +0800

    Added connection to cloud sql

--- db/db_connection.py
+++ db/db_connection.py
@@ -1,11 +1,31 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
 # Database URL (Update as needed)
 DATABASE_URL = "postgresql://postgres:password@localhost:5432/dbname"
 
 # Set up SQLAlchemy Engine and Base
-engine = create_engine(DATABASE_URL)
+# engine = create_engine(DATABASE_URL)
+db_user = os.environ.get["DB_USER"]
+db_pass = os.environ.get["DB_PASS"]
+db_name = os.environ.get["DB_NAME"]
+unix_socket_path = os.environ.get["INSTANCE_UNIX_SOCKET"]
+create_engine(
+        # Equivalent URL:
+        # postgresql+pg8000://<db_user>:<db_pass>@/<db_name>
+        #                         ?unix_sock=<INSTANCE_UNIX_SOCKET>/.s.PGSQL.5432
+        # Note: Some drivers require the `unix_sock` query parameter to use a different key.
+        # For example, 'psycopg2' uses the path set to `host` in order to connect successfully.
+        sqlalchemy.engine.url.URL.create(
+            drivername="postgresql+pg8000",
+            username=db_user,
+            password=db_pass,
+            database=db_name,
+            query={"unix_sock": f"{unix_socket_path}/.s.PGSQL.5432"},
+        ),
+        # ...
+    )
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
 
